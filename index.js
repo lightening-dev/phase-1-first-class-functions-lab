089534364e7f5ff87b03f4ba7a63ec4c1eaf3ad0
Lightening Gibson <lighteningibson@gmail.com>
2021-12-06 08:14:22 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers) {
+
+    return [drivers[0], drivers[1]];
+    
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    
+    return [drivers[2], drivers[3]];
+}
+    
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(factor) {
+    return function (x) {
+        return x * factor;
+        }
+    }
+
+
+let fareDoubler = createFareMultiplier(2);
+let fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, selectingDrivers) { 
+  return selectingDrivers(drivers);
+}
+    
+
